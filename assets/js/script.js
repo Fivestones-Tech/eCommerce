6ac7e557080f158193de7c459c4eba7837f84ef2
ogunnaike folawe matthew <fivestonestech@gmail.com>
2023-06-08 16:35:13 +0100

    //CSS and JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,31 @@ toastCloseBtn.addEventListener('click', function () {
     notificationToast.classList.add('closed');
 });
  
+ 
+ 
+ 
+ 
+ // Mobile menu variables
+ const mobileMenuOpenBtn = document.querySelector('[data-mobile-menu-open-btn]');
+ const mobileMenu = document.querySelectorAll('[data-mobile-menu]')
+ const mobileMenuCloseBtn = document.querySelectorAll('[ data-mobile-menu-close-btn]');
+ const overlay = document.querySelector('[data-overlay]');
+ 
+ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+     
+     //mobile menu function
+     const mobileMenuCloseFunc = function () {
+         mobileMenu[i].classList.remove('active');
+         overlay.classList.remove('active');
+     }
+     
+     mobileMenuOpenBtn[i].addEventListener('click', function () {
+         mobileMenu[i].classList.add('active');
+         overlay.classList.add('active');
+     });
+     
+     mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+     overlay.addEventListener('click', mobileMenuCloseFunc);
+     
+ }
+ 
